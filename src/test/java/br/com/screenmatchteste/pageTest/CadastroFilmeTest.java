c74304a5b7d4372e8f1a01870a85fe6f384f40b0
jeanheberth <jeanheberth19@gmail.com>
2023-06-19 22:37:34 -0300

    Alterando o nome do metodo CadastroFilmeTest.java

--- src/test/java/br/com/screenmatchteste/pageTest/CadastroFilmeTest.java
+++ src/test/java/br/com/screenmatchteste/pageTest/CadastroFilmeTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CadastroPageTest {
+public class CadastroFilmeTest {
 
     CadastroPage cadastroPage;
     ListarPage listarPage;
